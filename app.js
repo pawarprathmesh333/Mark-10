600d508dbd909ec6d9e952c0d7f3ee3e9216cf22
prathmesh <71115267+pawarprathmesh333@users.noreply.github.com>
2022-11-29 18:54:27 +0530

    Bill register app

---
+++ app.js
@@ -0,0 +1,37 @@
+    const billAmount = document.querySelector("#bill-amount");
+    const cashGiven = document.querySelector("#cash-given");
+    const checkButton = document.querySelector("#check");
+    const errorMessage = document.querySelector("#error-message");
+    const noofNotes = document.querySelectorAll(".no-of-notes");
+
+    const notes = [2000,500,100,20,10,5,1];
+
+    checkButton.addEventListener("click",function () {
+         errorMessage.style.display = "none";
+        if(Number(billAmount.value) > 0){
+            if(Number(cashGiven.value) >= Number(billAmount.value)){
+                var amountToReturn = Number(cashGiven.value) - Number(billAmount.value);
+                calculateChange(amountToReturn);
+            }
+            else{
+                showMessage("Cash given should at least equal to Bill Amount");
+            }
+        }
+        else{
+            showMessage("Bill Amount should be greater than 0.");
+        }
+    });
+
+    function showMessage(message){
+        errorMessage.style.display = "block";
+        errorMessage.innerText = message;
+    }
+
+    function calculateChange(amountToReturn){
+      
+        for(let i=0;i<notes.length;i++){
+            var noteTOReturn = Math.trunc(amountToReturn/notes[i]);
+            amountToReturn = amountToReturn%notes[i];
+            noofNotes[i].innerText = noteTOReturn;
+        }
+    }
